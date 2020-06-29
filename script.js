5b8e0247360273e295ad1adec1078b71f61e1adb
Sergey <1@mail.ru>
2020-06-29 18:56:47 +0300

    do challenge 2.4

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Я JavaScript!");
+// alert("Я JavaScript!");
+
+//2.4 challenge
+// let admin;
+// let userName = 'Джон';
+// admin = userName;
+// alert(admin);
+
+//2.4
+// let ourPlanetName = 'Земля';
+// let currentUserName = 'Джон';
