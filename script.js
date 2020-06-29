a90ae8514ec75db17e8cd42477e72c2915e954eb
Sergey <1@mail.ru>
2020-06-29 21:40:07 +0300

    do challenge 2.5

--- script.js
+++ script.js
@@ -9,3 +9,13 @@
 //2.4
 // let ourPlanetName = 'Земля';
 // let currentUserName = 'Джон';
+
+//2.5 challenge
+// let name = "Ilya";
+
+// alert( `hello ${1}` ); // hello 1
+
+// alert( `hello ${"name"}` ); // hello "name"
+
+// alert( `hello ${name}` ); // hello Ilya
+
